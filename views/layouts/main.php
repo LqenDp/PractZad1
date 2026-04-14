92c214482664d02574189ae4330318a7b42a5e83
LqenDp <oorzaksancaj7@gmail.com>
2026-04-14 22:42:59 +0700

    templates

---
+++ views/layouts/main.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Main site</title>
+</head>
+<body>
+    <div>
+        <?= $content ?? ''; ?>
+    </div>
+</body>
+</html>
